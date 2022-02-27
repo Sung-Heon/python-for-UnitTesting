40cb08c440931abb1dd157ff40879406b2ec208f
Sungheon Jeong <elon4856@gmail.com>
2022-02-27 13:15:33 +0900

    Update Listing1_1.py

--- Listing1_1.py
+++ Listing1_1.py
@@ -1,9 +1,14 @@
-def IsStringLong(input : str):
-  if len(input) > 5:
-    return True
-  return False
+def IsStringLong(input: str):
+    if len(input) > 5:
+        return True
+    return False
 
 
-def Test():
-  result = IsStringLong("abc")
-  assert False == result
+def test_IsStringLong():
+    result = IsStringLong("abc")
+    assert False == result
+
+
+if __name__ == "__main__":
+    test_IsStringLong()
+    print("Everything passed")
